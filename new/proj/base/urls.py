ce071e4c1bf1a6f090b8b06cbc7b6d316ec49096
k29laksh <klaxay0505@gmail.com>
2023-12-31 14:01:34 +0530

    updated

--- new/proj/base/urls.py
+++ new/proj/base/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('index',views.index,name='index'),
-    path('login',views.log_in,name='login'),
-    path('register',views.register,name='register')
+   
+    path('',views.index,name='index'),
+    path('/login',views.log_in,name='login'),
+    path('/register',views.register,name='register')
 ]
