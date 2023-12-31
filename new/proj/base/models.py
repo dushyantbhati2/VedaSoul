90bda9e852f8ccbe343791f3cbf7395847415be9
dushyantbhati2 <db90054@gmail.com>
2023-12-31 13:16:36 +0530

    updated

--- new/proj/base/models.py
+++ new/proj/base/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
 # Create your models here.
+User=get_user_model()
+
+class Profile(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    id_user=models.IntegerField()
+    location=models.CharField(max_length=200,blank=True)
+    bio=models.TextField(blank=True)
+    profimag=models.ImageField(upload_to="prof_images")
+    
+    def __str__(self):
+        return self.user.username
+
+class Post(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4)
+    user=models.CharField(max_length=100)
+    image=models.ImageField(upload_to="posts")
+    caption=models.CharField(max_length=100)
+    location=models.CharField(max_length=200)
+    upload_time=models.DateTimeField(default=datetime.now)
+    likes=models.IntegerField(default=0)
+    def __str__(self):
+        return self.user
+    
