2eb93f5cb5df428a0a2c0f004deb2e830933799a
k29laksh <klaxay0505@gmail.com>
2024-01-01 22:00:12 +0530

    updated

---
+++ new/proj/base/static/js/home.js
@@ -0,0 +1,22 @@
+
+var divp = document.getElementById("hidden-tweet");
+function togglep(){
+  //  divp.classList.toggle("hidden_tweet_box");
+  //  divp.classList.toggle(".hidden-main-input");
+  //  divp.classList.toggle(".hidden-post-btn");
+  //  divp.classList.toggle(".hidden-Menu_options");
+
+  if (divp.style.display === "none"){
+      divp.style.display= "block";
+  }
+  else {
+    divp.style.display="none";
+  }
+ 
+  
+}
+function inputmeathod(){
+ 
+   document.getElementById("file_input1").click();
+  
+}
